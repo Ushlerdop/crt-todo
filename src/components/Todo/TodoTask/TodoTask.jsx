defa3df944e03c98c68fd317d125fee51caa8c46
Ushlerdop <axalimon@gmail.com>
2022-01-20 17:34:46 +1000

    Now TodoTask is a functional component
    Переписал TodoTask в функциональный компонент

--- src/components/Todo/TodoTask/TodoTask.jsx
+++ src/components/Todo/TodoTask/TodoTask.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useContext, useEffect, useState } from 'react';
 import ChangeTaskButton from '../../UI/buttons/ChangeTaskButton/ChangeTaskButton';
 import DeleteButton from '../../UI/buttons/DeleteButton/DeleteButton';
 import DoneButton from '../../UI/buttons/DoneButton/DoneButton';
@@ -12,102 +12,85 @@ import PropTypes from 'prop-types';
 
 const cx = classNames.bind(styles);
 
-class TodoTask extends Component {
-  constructor(props) {
-    super(props);
-    this.state={
-      isEditModalActive: false,
-      isInfoModalActive: false,
-    }
-    this.setEditModalActive = this.setEditModalActive.bind(this);
-    this.setInfoModalActive = this.setInfoModalActive.bind(this);
-  }  
+function TodoTask(props) {
+  const [isEditModalActive, setIsEditModalActive] = useState(false);
+  const [isInfoModalActive, setIsInfoModalActive] = useState(false);
 
-  setEditModalActive(value) {
-    this.setState({
-      isEditModalActive: value
-    })
+  const { language } = useContext(LanguageContext);
+
+  const setEditModalActive = (value) => {
+    setIsEditModalActive(value);
   }
 
-  setInfoModalActive(value) {
-    this.setState({
-      isInfoModalActive: value
-    })
+  const setInfoModalActive = (value) => {
+    setIsInfoModalActive(value);
   }
-  
-  render() {
-    const taskClassName = cx({
-      task: true,
-      taskImportant: this.props.isImportant,
-      taskDone: this.props.isDone,
-    });
 
-    const titleTaskClassName = cx({
-      taskTitle: true,
-      taskTitleDone: this.props.isDone,
-    });
+  const taskClassName = cx({
+    task: true,
+    taskImportant: props.isImportant,
+    taskDone: props.isDone,
+  });
 
-    const descriptionTaskClassName = cx({
-      taskDescription: true,
-      taskDescriptionDone: this.props.isDone,
-    });
+  const titleTaskClassName = cx({
+    taskTitle: true,
+    taskTitleDone: props.isDone,
+  });
 
-    return (
-      <LanguageContext.Consumer>
-        {
-          ({ language }) => (
-            <li className={styles.taskContainer}>
-              <TodoUpdateForm
-                active={this.state.isEditModalActive}
-                setModalActive={this.setEditModalActive}
-                {...this.props}
-              />
-              <TodoTaskModal
-                active={this.state.isInfoModalActive}
-                setModalActive={this.setInfoModalActive}
-                {...this.props}
-              />
-              <div className={taskClassName}  onClick={() => this.setInfoModalActive(true)}>
-                <div className={styles.taskInfo}>
-                  <div className={titleTaskClassName}>
-                    {this.props.title}
-                  </div>
-                  <div className={descriptionTaskClassName}>
-                    {this.props.description}
-                  </div>
-                  <div className={styles.taskDate}>
-                    {`${language.task.edited} ${this.props.editedDate}`}
-                  </div>
-                </div>
-                <div 
-                  className={styles.taskControl} 
-                  onClick={(e) => e.stopPropagation()}
-                >
-                  <DeleteButton 
-                    deleteTask={this.props.deleteTask}
-                    id={this.props.id}
-                  />
-                  <DoneButton 
-                    isTaskPropertyToggle={this.props.isTaskPropertyToggle}
-                    id={this.props.id}
-                    isDone={this.props.isDone}
-                  />
-                  <ImportantButton
-                    isTaskPropertyToggle={this.props.isTaskPropertyToggle}
-                    id={this.props.id}
-                    isImportant={this.props.isImportant}
-                  />
-                  <ChangeTaskButton
-                    setEditModalActive={this.setEditModalActive}
-                  />
-                </div>
-              </div>
-            </li>
-          )
-        }
-      </LanguageContext.Consumer>
-    );
-  }
+  const descriptionTaskClassName = cx({
+    taskDescription: true,
+    taskDescriptionDone: props.isDone,
+  });
+
+  return (
+    <li className={styles.taskContainer}>
+      <TodoUpdateForm
+        active={isEditModalActive}
+        setModalActive={setEditModalActive}
+        {...props}
+      />
+      <TodoTaskModal
+        active={isInfoModalActive}
+        setModalActive={setInfoModalActive}
+        {...props}
+      />
+      <div className={taskClassName} onClick={() => setInfoModalActive(true)}>
+        <div className={styles.taskInfo}>
+          <div className={titleTaskClassName}>
+            {props.title}
+          </div>
+          <div className={descriptionTaskClassName}>
+            {props.description}
+          </div>
+          <div className={styles.taskDate}>
+            {`${language.task.edited} ${props.editedDate}`}
+          </div>
+        </div>
+        <div
+          className={styles.taskControl}
+          onClick={(e) => e.stopPropagation()}
+        >
+          <DeleteButton
+            deleteTask={props.deleteTask}
+            id={props.id}
+          />
+          <DoneButton
+            isTaskPropertyToggle={props.isTaskPropertyToggle}
+            id={props.id}
+            isDone={props.isDone}
+          />
+          <ImportantButton
+            isTaskPropertyToggle={props.isTaskPropertyToggle}
+            id={props.id}
+            isImportant={props.isImportant}
+          />
+          <ChangeTaskButton
+            setEditModalActive={setEditModalActive}
+          />
+        </div>
+      </div>
+    </li>
+  )
 }
 
 TodoTask.propTypes = {
